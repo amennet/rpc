ca94f8cdca61fbbaab5911284ec4132f6afc719a
zhangge <zhangge55@126.com>
2016-06-14 16:03:00 +0800

    init rpc framework

---
+++ src/main/java/rpc/sample/server/RpcBootstrap.java
@@ -0,0 +1,15 @@
+package rpc.sample.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class RpcBootstrap {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcBootstrap.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("start rpc.server");
+        new ClassPathXmlApplicationContext("spring.xml");
+    }
+}
